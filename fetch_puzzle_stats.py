0e97afcda4a8d2e1112a7d8e925c3e56898215dc
jvp119 <jayvanput@gmail.com>
2023-04-18 23:59:43 -0400

    Add request fetching

--- fetch_puzzle_stats.py
+++ fetch_puzzle_stats.py
@@ -1,11 +1,19 @@
 import dotenv
 import os
+import datetime
 
 from stat_fetcher import StatFetcher
 
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
+
+    # Get cookies
     COOKIE = os.environ.get("COOKIE")
     fetcher = StatFetcher(COOKIE)
-    fetcher.fetch_stats()
+
+    # get today's date.
+    todays_date = datetime.date.today()
+    output = fetcher.fetch_stats(todays_date)
+
+    print(output)
--- stat_fetcher.py
+++ stat_fetcher.py
@@ -1,15 +1,40 @@
 import os
-
+import datetime
+import requests
+import dotenv
 
 class StatFetcher:
 
-    def __init__(self, cookie):
-        self.cookie: str|None = cookie
+    PUZZLE_INFO = "https://nyt-games-prd.appspot.com/svc/crosswords/v2/puzzle/daily-{date}.json"
+    SOLVE_INFO = "https://nyt-games-prd.appspot.com/svc/crosswords/v2/game/{puzzle_id}.json"
 
-    def fetch_stats(self):
-        print(self.cookie)
+    def __init__(self, cookie):
+        self.cookie: dict[str, str|None] = {'NYT-S': cookie}
+
+    def get_puzzle_id(self, puzzle_date: datetime.date) -> str:
+        """ Returns the puzzle ID for the specified date. """
+        date_str = puzzle_date.strftime("%Y-%m-%d")
+        response = requests.get(self.PUZZLE_INFO.format(date=date_str))
+        response.raise_for_status()
+
+        puzzle_id = response.json()["results"][0]["puzzle_id"]
+
+        return puzzle_id
+
+    def fetch_stats(self, puzzle_date: datetime.date) -> dict[str, str]:
+        """ Returns the user's stats for the specified date. """
+        puzzle_id = self.get_puzzle_id(puzzle_date)
+        response = requests.get(
+            self.SOLVE_INFO.format(puzzle_id=puzzle_id),
+            cookies=self.cookie    
+        )
+        return response.json()["results"]
 
 
 if __name__ == "__main__":
-    stat_fetcher = StatFetcher()
-    stat_fetcher.fetch_stats()
+    dotenv.load_dotenv()
+    COOKIE = os.environ.get("COOKIE")
+    fetcher = StatFetcher(COOKIE)
+    stats = fetcher.fetch_stats(datetime.date(2023, 4, 19))
+
+    print(stats)
